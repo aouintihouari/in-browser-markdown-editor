8bb2f75ea2c721d04007640046dd13418d0e4d17
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-06-26 15:21:00 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,29 +1,30 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import documentsRoute from "./routes/documents.route.js";
 
-const app = express();
+dotenv.config();
 
-app.use(express.json());
+const app = express();
 
 const allowedOrigin = process.env.CORS_ORIGIN;
 
-app.use(cors(
+app.use(cors({
   origin: allowedOrigin,
   credentials: true
-));
+}));
+
+app.use(express.json());
 
 app.use("/api/v1/documents", documentsRoute);
 
 app.use((err, req, res, next) => {
   console.error("ERROR 💥", err);
-  res
-    .status(err.statusCode || 500)
-    .json({
-      status: err.status || "error",
-      message: err.message || "Something went wrong",
-    });
+  res.status(err.statusCode || 500).json({
+    status: err.status || "error",
+    message: err.message || "Something went wrong",
+  });
 });
 
 export default app;
